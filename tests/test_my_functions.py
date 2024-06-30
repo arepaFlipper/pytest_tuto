da4fc1bbf59d6fd700542b87c6d28e4bc2b4e7a5
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-06-30 07:12:18 -0500

    ❌Test functions add and divide.

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -2,6 +2,9 @@ import pytest
 import source.my_functions as my_functions
 
 def test_add():
-    pass
+    result = my_functions.add(1, 4)
+    assert result == 5
 
-
+def test_divide():
+    result = my_functions.divide(10, 5)
+    assert result == 2
