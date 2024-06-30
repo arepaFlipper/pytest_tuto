6c6e1eb596fef7b2ceef24098840cbafdaf0e390
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-06-30 13:06:16 -0500

    🛅 test the area of a circle

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,16 +1,17 @@
-import pytest
+import pytest, math
 import source.shapes as shapes
 
 class TestCircle:
 
     def setup_method(self, method):
         print(f"Setting up {method}👈 ")
+        self.circle = shapes.Circle(10)
 
     def teardown_method(self, method):
         print(f"Tearing down {method} 🥲 ")
 
-    def test_one(self):
-        assert 1 == 1
+    def test_area(self):
+        assert self.circle.area() == math.pi * self.circle.radius ** 2
 
     def test_two(self):
         assert 2 == 2
