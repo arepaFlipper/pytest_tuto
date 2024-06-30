b5dff5f43105c24fd15f5ae567e51d5949ff0fb7
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-06-30 18:06:46 -0500

    🚪 use fixture decorator to re-use the Rectangle shape

---
+++ tests/test_rectangle.py
@@ -0,0 +1,12 @@
+import pytest, math
+import source.shapes as shapes
+
+@pytest.fixture
+def my_rectangle():
+    return shapes.Rectangle(10, 20)
+
+def test_area(my_rectangle):
+    assert my_rectangle.area() == 10 * 20
+
+def test_perimeter(my_rectangle):
+    assert my_rectangle.perimeter() == (10*2) + (20*2)
